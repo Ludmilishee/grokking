4de7f7f19f59a90077f66a017bc883595fd2ee15
Ludmilishee <luda_babich@mail.ru>
2021-01-19 00:00:42 +0500

    add chapter 3 find in box & gitignore

--- chapter_3/find_in_boxes.js
+++ chapter_3/find_in_boxes.js
@@ -1,8 +1,32 @@
+function findInChestCyclic(chest, key) {
+    const boxes = chest.slice();
+
+    while (boxes.length) {
+        const next = boxes[0];
+
+        if (Array.isArray(next)) {
+            boxes.push(...next);
+        } else if (next === key) {
+            return next;
+        }
+        boxes.shift();
+    }
+}
+
+function findInChestRecursively(box, key) {
+    box.find(item => {
+        if (Array.isArray(item)) {
+            return findInChestRecursively(item, key)
+        } else {
+            return item === key;
+        }
+    });
+}
+
 // Имитация коробок в коробках
 // [] - коробка в сундуке
 // null - какая-то вещь
 // 'key' - ключ, который необходимо найти
-
 const chest = [
     [ null, null, null, null ],
     [
@@ -15,3 +39,6 @@ const chest = [
         [ null, [ null, null ] ],
     ],
 ];
+
+findInChestCyclic(chest, 'key');
+findInChestRecursively(chest, 'key');
