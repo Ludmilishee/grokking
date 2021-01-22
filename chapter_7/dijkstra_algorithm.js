8f12d1f67d84c477d0a9ed481114f34a1544eebf
Ludmilishee <luda_babich@mail.ru>
2021-01-22 13:35:32 +0500

    chapter 7 add dijkstra algorithm

---
+++ chapter_7/dijkstra_algorithm.js
@@ -0,0 +1,76 @@
+function findCheapestPath(graph) {
+    const { costs, parents } = initFillTables();
+    const processed = [];
+
+    let node = findNearestNode(costs, processed);
+    while (node) {
+        const nodeCost = costs[node];
+
+        Object.keys(graph[node]).forEach(n => {
+            const newValue = nodeCost + graph[node][n];
+            if (newValue < costs[n]) {
+                costs[n] = newValue;
+                parents[n] = node;
+            }
+        });
+        processed.push(node);
+        node = findNearestNode(costs, processed);
+    }
+
+    return {
+        summaryCost: costs.fin,
+        path: getPath(parents)
+    }
+}
+
+function initFillTables() {
+    const costs = { fin: Infinity };
+    const parents = {};
+
+    Object.assign(costs, graph.start);
+    Object.keys(graph.start).forEach(n => {
+        parents[n] = 'start';
+    });
+
+    return { costs, parents };
+}
+
+function findNearestNode(costs, processed) {
+    let nearest = null;
+
+    Object.keys(costs).forEach(n => {
+        if ((nearest === null || costs[n] < costs[nearest]) && !processed.includes(n)) {
+            nearest = n;
+        }
+    });
+
+    return nearest;
+}
+
+function getPath(parents) {
+    let res = 'fin';
+    let nextValue = parents.fin;
+
+    while (nextValue) {
+        res = `${nextValue} -> ${res}`;
+        nextValue = parents[nextValue];
+    }
+
+    return res;
+}
+
+const graph = {
+    start: {
+        a: 6,
+        b: 2
+    },
+    a: {
+        fin: 1
+    },
+    b: {
+        a: 3,
+        fin: 5
+    },
+    fin: {}
+};
+console.log(findCheapestPath(graph)); // { summaryCost: 6, path: 'start -> b -> a -> fin' }
